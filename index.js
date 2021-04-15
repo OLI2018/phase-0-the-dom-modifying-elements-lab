e4cc1dc9dcfb98c49a2c941beaac3ea6636cea40
Oleg <roxvemm2014@gmail.com>
2021-04-14 22:03:48 -0700

    Complented assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove(); 
+
+const newHeader= document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id='victory';
+
+newHeader.innerHTML="<h1>Saloni is the champion </h1>"
+newHeader.style.backgroundColor='#27647b';
